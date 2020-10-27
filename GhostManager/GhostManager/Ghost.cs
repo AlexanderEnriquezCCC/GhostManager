59141865cb18df10f38db5e23d2d8e4e4fda68a3
AlexanderEnriquezCCC <44684320+AlexanderEnriquezCCC@users.noreply.github.com>
2020-10-27 18:27:31 -0500

    adds logic to Ghost

--- GhostManager/GhostManager/Ghost.cs
+++ GhostManager/GhostManager/Ghost.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace GhostManager
 {
+    public enum GhostState { Chasing, Evading, Roving, Dead} //only need 2 really, roving for movement and dead to change how ghost looks.
     class Ghost
     {
+        GhostState gState;
+
+        public GhostState State{
+            get { return gState; }
+            set
+            {
+                if(gState != value)
+                {
+                    this.Log(string.Format("{0} was: {1} now {2}", this.ToString(), gState, value));
+                }
+            }
+        }
     }
 }
